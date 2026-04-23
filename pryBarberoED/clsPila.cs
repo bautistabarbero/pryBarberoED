1c1028d340a60615a145364efd3fdbcd33882d62
bautistabarbero <bautibarbero97@gmail.com>
2026-04-23 12:49:46 -0300

    cambios de hoy

--- pryBarberoED/clsPila.cs
+++ pryBarberoED/clsPila.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace pryBarberoED
 {
     internal class clsPila
     {
-    }
+        public class clsPila
+        {
+            // Campo de la clase
+            private clsNodo pri;
+
+            // Propiedad
+            public clsNodo Primero
+            {
+                get { return pri; }
+                set { pri = value; }
+            }
+
+            // Método para agregar a la pila
+            public void Agregar(clsNodo Nuevo)
+            {
+                if (Primero == null)
+                {
+                    Primero = Nuevo;
+                }
+                else
+                {
+                    Nuevo.Siguiente = Primero;
+                    Primero = Nuevo;
+                }
+            }
+
+            // Método para eliminar de la pila
+            public void Eliminar()
+            {
+                if (Primero != null)
+                {
+                    Primero = Primero.Siguiente;
+                }
+            }
+
+            // Recorrer y mostrar en DataGridView
+            public void Recorrer(DataGridView Grilla)
+            {
+                clsNodo aux = Primero;
+                Grilla.Rows.Clear();
+
+                while (aux != null)
+                {
+                    Grilla.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                    aux = aux.Siguiente;
+                }
+            }
+
+            // Recorrer y guardar en archivo de texto
+            public void Recorrer(string NmbreArchivo)
+            {
+                clsNodo aux = Primero;
+                StreamWriter AD = new StreamWriter(NmbreArchivo, false, Encoding.UTF8);
+
+                AD.WriteLine("Lista de personas\n");
+                AD.WriteLine("Código;Nombre;Trámite");
+
+                while (aux != null)
+                {
+                    AD.Write(aux.Codigo);
+                    AD.Write(";");
+                    AD.Write(aux.Nombre);
+                    AD.Write(";");
+                    AD.WriteLine(aux.Tramite);
+                    aux = aux.Siguiente;
+                }
+
+                AD.Close();
+            }
+        }
 }
